48c54d11d7bd24583947243a2caf61350d4887a7
Yurii <yurij.kenu@gmail.com>
2026-01-01 17:48:35 +0100

    main: draft

--- demo-service/src/main/java/com/keniu/DemoApplication.java
+++ demo-service/src/main/java/com/keniu/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CircuitBreakerApplication {
+public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CircuitBreakerApplication.class, args);
+	static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
---
+++ unstable-service/src/main/java/com/keniu/UnstableApplication.java
@@ -0,0 +1,13 @@
+package com.keniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstableApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(UnstableApplication.class, args);
+	}
+
+}
